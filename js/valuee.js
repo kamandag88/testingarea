bf4d04e8b632c8f6ae89b95e88bcce8738d7b984
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-11-24 08:59:18 +0800

    Update valuee.js

--- js/valuee.js
+++ js/valuee.js
@@ -1,55 +1,14 @@
-        setup();
+    var val= 0;
+    var valstring = "";
+    var dwdstr = "";
 
-        var value;
-        var score;
-        var count;
-
-        count=0;
-        score=0;
-
-        function setup(){
-        if(cpf)
-            cpf.setPinMode('["resetPin"],["setPinMode", "digital", 2,"INPUT"]');
+        function uploadd(){
+            val = document.getElementById("val").innerHTML;
+            valstring = val.toString();
+            aop.setStore(store0, valstring)
         }
 
-
-
-        function loop(){
-
-            if(cpf){
-
-                value = cpf.get("d2");
-                document.getElementById("val").innerHTML = value; 
-
-                if(value == 1 && count == 0) {
-                    setbackg();
-                    addscore();
-                    addcounter();
-                }
-                else{
-                    setbackgback();
-                }
-            }
-                setTimeout("loop()", 1000); 
-        }
-
-        loop();
-
-
-        function setbackg(){
-            document.body.style.backgroundImage = "url('img/InsertCoverInfo.jpg')";
-        }
-
-        function setbackgback(){
-            document.body.style.backgroundImage = "url('img/6thScreen.jpg')";
-        }
-
-        function addscore(){
-            score = score + 100;
-            document.getElementById("vall").innerHTML = score;
-        }
-
-        function addcounter(){
-            count = count + 1;
-            document.getElementById("valll").innerHTML = count;
+        function downloadd(){
+            dwdstr = aop.GetStore(store0);
+            document.getElementById("vallll").innerHTML = dwdstr;
         }
