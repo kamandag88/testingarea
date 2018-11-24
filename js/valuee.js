248cd7059b8704a6beecf7b9c08ea7bfd8526c6d
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-11-24 09:50:19 +0800

    Update valuee.js

--- js/valuee.js
+++ js/valuee.js
@@ -1,16 +1,20 @@
     var val= 0;
+    var valarray = [];
+    var json = [];
     var valstring = "";
     var dwdstr = "";
     var valdwd = 0;
 
         function uploadd(){
             val = document.getElementById("val").innerHTML;
-            valstring = JSON.stringify(val);
+            valarray = {"value": val};
+            json.push(valarray);
+            valstring = JSON.stringify(json);
             aop.setStore("store0", valstring)
         }
 
         function downloadd(){
-            dwdstr = aop.getStore("store0");
+            dwdstr = aop.getStore("store0");    
             valdwd = JSON.parse(dwdstr);
-            document.getElementById("vallll").innerHTML = valdwd;
+            document.getElementById("vallll").innerHTML = valdwd.value;
         }
